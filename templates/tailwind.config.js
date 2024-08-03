cad05f2b50daeb58c5944596f85cc29ae3446f9c
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-08-03 22:53:52 +0600

    Initial commit with completed templates

---
+++ templates/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./html/*'],
+  theme: {
+    extend: {
+      width:{
+        'login-width': '360px',
+        'full': '100%',
+      },
+    
+    },
+  },
+  plugins: [],
+}
+
